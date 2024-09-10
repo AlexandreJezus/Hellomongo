c60970e03e7b0dfeeb5336daab36a6e639a73501
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-09-10 11:15:02 -0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 const port = 5000;
 app.use(express.json());
-app.listen(port, () => console.log("Servidor escutando"));
+app.listen(port, () => console.log("Servidor escutando na porta " + port));
